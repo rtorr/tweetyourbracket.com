9d0c4ff5bbd0b9a610cc306758f3cd20cbf73d2e
Luke Karrys <lukekarrys@gmail.com>
2014-01-25 14:55:10 -0700

    new page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+/* global moment, countdown */
 
+var $c = $('.countdown'),
+    m = moment('2014-03-16T23:00:00.000Z');
+    setTime = function () {
+        var time = m.countdown(
+            new Date(),
+            countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS,
+            NaN,
+            0
+        ).toString();
+        $c.html(time.replace(/, (and )?/g, '<br>'));
+        $('title').text('Tweet Your Bracket | ' + time);
+    };
+
+setTime();
+setInterval(setTime, 1000);
